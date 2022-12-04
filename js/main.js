732434c243b83c54795d4ae3d29b8fd8d812e648
suuu0122 <suuu0122lab@gmail.com>
2022-12-04 21:13:15 +0900

    add js

---
+++ js/main.js
@@ -0,0 +1,82 @@
+app = Vue.createApp({
+	data() {
+		return {
+			initialPage: true,
+			createTaskPage: false,
+			checkTasksPage: false,
+			userName: "",
+			taskName: "",
+			taskContents: "",
+			taskList: {}
+		}
+	},
+
+	methods: {
+		create_task() {
+			this.taskList[this.taskName] = this.taskContents;
+			this.taskName = "";
+			this.taskContents = "";
+		},
+
+		move_initialPage() {
+			this.initialPage = true;
+			this.createTaskPage = false;
+			this.checkTasksPage = false;
+		},
+
+		move_createTasksPage() {
+			this.initialPage = false;
+			this.createTaskPage = true;
+			this.checkTasksPage = false;
+		},
+
+		move_checkTasksPage() {
+			this.initialPage = false;
+			this.createTaskPage = false;
+			this.checkTasksPage = true;
+		},
+
+		delete_task(key) {
+			delete this.taskList[key];
+		},
+
+		save_data() {
+			let taskListEncoded = JSON.stringify(this.taskList);
+			localStorage.setItem(this.userName, taskListEncoded);
+			this.move_initialPage();
+		},
+
+		delete_data() {
+			let userTaskList = localStorage.getItem(this.userName);
+			if (userTaskList === null) {
+				alert("No data in local storage");
+			} else {
+				localStorage.removeItem(this.userName);
+				this.taskList = {};
+				this.move_initialPage();
+			}
+		},
+
+		new_taskList() {
+			if (this.userName === "") {
+				alert("Please input your name");
+			} else {
+				this.move_createTasksPage();
+			}
+		},
+
+		login_taskList() {
+			let taskListEncoded = localStorage.getItem(this.userName);
+			if (taskListEncoded === null) {
+				alert("Your data is None");
+			} else {
+				let taskListDecoded = JSON.parse(taskListEncoded);
+				this.taskList = taskListDecoded;
+				this.move_checkTasksPage();
+			}
+		}
+
+	}
+})
+
+app.mount('#app')
